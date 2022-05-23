a249363569e62519325a08eb0c6da15213d5e986
KeerthanKumar Jojoda <keerthan.j@westagilelabs.com>
2022-05-23 16:27:51 +0530

    Added files and folders

--- node/routes/department.js
+++
@@ -1,8 +0,0 @@
-var express = require("express");
-var router = express.Router();
-const departmentController = require("../controllers/department");
-router.get("/", departmentController.getDepartments);
-router.post("/", departmentController.createDepartment);
-router.delete("/:id", departmentController.deleteDepartment);
-router.put("/:id", departmentController.updateDepartment);
-module.exports = router;
--- node/routes/user.js
+++
@@ -1,7 +0,0 @@
-var express = require("express");
-var router = express.Router();
-const userController = require("../controllers/user");
-router.get("/", userController.getUsers);
-router.post("/", userController.createUser);
-router.post("/login", userController.loginUser);
-module.exports = router;
